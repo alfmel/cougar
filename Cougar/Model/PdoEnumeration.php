43595e1bc8bd88da1978bc43d35e9bad83823678
Alberto Trevino <alberto@byu.edu>
2013-11-19 15:52:26 -0700

    Fix PdoEnumerator to get column metadata on columns; update cougar-model-builder to build model files more like the ones in the tutorial

--- Cougar/Model/PdoEnumeration.php
+++ Cougar/Model/PdoEnumeration.php
@@ -3,6 +3,7 @@
 namespace Cougar\Model;
 
 use PDO;
+use Cougar\Util\Format;
 use Cougar\Exceptions\Exception;
 
 # Initialize the framework
@@ -11,15 +12,18 @@ require_once("cougar.php");
 /**
  * @history
  * 2013.09.30:
- *   (AT)  Initial release
+ *   (JPK) Initial release
+ * 2013.11.19:
+ *   (AT)  Fix issue with Oracle driver not supporting column metadata
  *
- * @version 2013.09.30
+ * @version 2013.11.19
  * @package Cougar
  * @license MIT
  *
  * @copyright 2013 Brigham Young University
  *
  * @author (JPK) Jillian Koontz, Brigham Young Univ. <jpkoontz@gmail.com>
+ * @author (AT) Alberto Trevino, Brigham Young Univ. <alberto@byu.edu>
  */
 class PdoEnumeration
 {
@@ -28,7 +32,7 @@ class PdoEnumeration
      *
      * @history
      * 2013.09.30:
-     *   (AT)  Initial release
+     *   (JPK) Initial release
      *
      * @version 2013.09.30
      * @author (JPK) Jillian Koontz, Brigham Young Univ. <jpkoontz@gmail.com>
@@ -47,7 +51,7 @@ class PdoEnumeration
      *
      * @history
      * 2013.09.30:
-     *   (AT)  Initial release
+     *   (JPK) Initial release
      *
      * @version 2013.09.30
      * @author (JPK) Jillian Koontz, Brigham Young Univ. <jpkoontz@gmail.com>
@@ -67,7 +71,7 @@ class PdoEnumeration
      *
      * @history
      * 2013.09.30:
-     *   (AT)  Initial release
+     *   (JPK) Initial release
      *
      * @version 2013.09.30
      * @author (JPK) Jillian Koontz, Brigham Young Univ. <jpkoontz@gmail.com>
@@ -125,10 +129,13 @@ class PdoEnumeration
      *
      * @history
      * 2013.09.30:
-     *   (AT)  Initial release
+     *   (JPK) Initial release
+     * 2013.11.19:
+     *   (AT)  Work around OCI driver limitations to obtain column information
      *
-     * @version 2013.09.30
+     * @version 2013.11.19
      * @author (JPK) Jillian Koontz, Brigham Young Univ. <jpkoontz@gmail.com>
+     * @author (AT) Alberto Trevino, Brigham Young Univ. <alberto@byu.edu>
      *
      * @param string $table Table name
      * @throws \Cougar\Exceptions\Exception
@@ -165,14 +172,62 @@ class PdoEnumeration
                 break;
             case "oci":
                 $statement = $this->pdo->prepare(
-                    "SELECT * FROM " . $table . " WHERE ROWNUM = 1");
-                $statement->execute();
-                $result = $statement->fetch();
+                    "SELECT COLUMN_NAME, DATA_TYPE, DATA_PRECISION, DATA_SCALE,
+							NULLABLE
+						FROM dba_tab_columns WHERE table_name = :table_name");
+                $statement->execute(array(":table_name" => $table));
+                $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
 
-                for ($i = 0; $i < count($result); $i++)
-                {
-                    $columns[] = $statement->getColumnMeta($i);
-                }
+				foreach($result as $raw_column)
+				{
+					$column = array("name" => "",
+						"flags" => array(),
+						"native_type" => ""
+					);
+
+					$column["name"] = $raw_column["COLUMN_NAME"];
+					switch($raw_column["DATA_TYPE"])
+					{
+						case "NUMBER":
+						case "LONG":
+						case "LONG RAW":
+							if ($raw_column["DATA_SCALE"] > 0)
+							{
+								$column["native_type"] = "float";
+							}
+							else
+							{
+								$column["native_type"] = "int";
+							}
+							break;
+						case "FLOAT":
+							$column["native_type"] = "float";
+							break;
+						case "DATE":
+						case "TIMESTAMP":
+							$column["native_type"] = "datetime";
+							break;
+						default:
+							if (substr($raw_column["DATA_TYPE"], 0, 9) ==
+								"TIMESTAMP")
+							{
+								$column["native_type"] = "datetime";
+							}
+							else
+							{
+								$column["native_type"] = "string";
+							}
+							break;
+					}
+
+					Format::strToBool($raw_column["NULLABLE"]);
+					if (! $raw_column["NULLABLE"])
+					{
+						$column["floags"][] = "not_null";
+					}
+
+					$columns[] = $column;
+				}
                 break;
             default:
                 throw new Exception("Unsupported driver");
